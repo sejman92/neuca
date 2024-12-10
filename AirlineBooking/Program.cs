5a70708e9b88e06572613a42ff77130415c22f2e
Mateusz Szymański <sejman92@gmail.com>
2024-12-10 15:20:45 +0100

    Initial commit.

---
+++ AirlineBooking/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Microsoft.Extensions.DependencyInjection;
+
+Console.WriteLine("Hello, World!");
+
+var serviceProvider = new ServiceCollection()
+    .BuildServiceProvider();
